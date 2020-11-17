519f3b3cf7619aaec0e618bab3b3693332babbd4
Crowdbotics <team@crowdbotics.com>
2020-11-17 04:51:14 +0000

    Initial commit.

---
+++ ios/mdi_22693Tests/mdi_22693Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface mdi_22693 : XCTestCase
+
+@end
+
+@implementation mdi_22693
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
